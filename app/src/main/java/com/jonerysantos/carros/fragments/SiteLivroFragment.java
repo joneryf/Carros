6db40efab54cfd7418bea7375359d77becad4b53
joneryf <joneryf@gmail.com>
2016-10-22 09:46:09 -0200

    Add swipe to refresh - cap 11.2

--- app/src/main/java/com/jonerysantos/carros/fragments/SiteLivroFragment.java
+++ app/src/main/java/com/jonerysantos/carros/fragments/SiteLivroFragment.java
@@ -2,7 +2,7 @@ package com.jonerysantos.carros.fragments;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +16,7 @@ public class SiteLivroFragment extends BaseFragment {
     private static final String URL_SOBRE = "http://www.livroandroid.com.br/sobre.htm";
     private WebView webview;
     private ProgressBar progress;
+    protected SwipeRefreshLayout swipeLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,7 +28,23 @@ public class SiteLivroFragment extends BaseFragment {
         setWebViewClient(webview);
         //Carrega a página
         webview.loadUrl(URL_SOBRE);
+        //Swipe to Refresh
+        swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeToRefresh);
+        swipeLayout.setOnRefreshListener(OnRefreshListener());
+        //Cores da animação
+        swipeLayout.setColorSchemeResources(
+                R.color.refresh_progress_1,
+                R.color.refresh_progress_2,
+                R.color.refresh_progress_3);
         return view;
+    }
+    private SwipeRefreshLayout.OnRefreshListener OnRefreshListener(){
+        return new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webview.reload();//Atualiza a página
+            }
+        };
     }
     private void setWebViewClient(WebView webview){
         webview.setWebViewClient(new WebViewClient(){
@@ -42,6 +59,8 @@ public class SiteLivroFragment extends BaseFragment {
             public void onPageFinished(WebView webview, String url) {
                 //Desliga o progress
                 progress.setVisibility(View.INVISIBLE);
+                //Termina a animação do Swipe to Refresh
+                swipeLayout.setRefreshing(false);
             }
         });
     }
